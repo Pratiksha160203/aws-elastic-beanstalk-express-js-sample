9f70dee28644e7a8454e92a2f1b9219c424ff9dc
Pratiksha Narasimha Nayak G <pratikshanng.cs20@rvce.edu.in>
2024-09-08 23:54:58 +0530

    Update message in the app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is my first AWS project. I am learning'));
+app.get('/', (req, res) => res.send('Hello World! This is my first AWS project. I am learning. Now, I have changed the message :)) - Review stage has been added!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
